815c3e5e55e8823cd0c7b76fe37f849ecae3f44a
fernando-freires <ffdc.snf21@uea.edu.br>
2024-08-31 18:35:37 -0400

    fix error in actions v2

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,23 +1,23 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "bucket_name" {
-    type = string
+  type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
-    bucket = "static_site-${var.bucket_name}"
+  bucket = "static_site-${var.bucket_name}"
 
-    website {
-        index_document = "index.html"
-        error_document = "404.html"
-    }
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 
-    tags = {
-        Name = "Static Site Bucket"
-        Environment = "Production"
-    }
+  tags = {
+    Name        = "Static Site Bucket"
+    Environment = "Production"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
@@ -37,11 +37,11 @@ resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "static_site_bucket" {
-    depends_on = [
-        aws_s3_bucket_public_access_block.static_site_bucket,
-        aws_s3_bucket_ownership_controls.static_site_bucket,
-    ]
+  depends_on = [
+    aws_s3_bucket_public_access_block.static_site_bucket,
+    aws_s3_bucket_ownership_controls.static_site_bucket,
+  ]
 
-    bucket = aws_s3_bucket.static_site_bucket.id
-    acl = "public-read"
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
 }
